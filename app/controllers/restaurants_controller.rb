ef07df831f1633b74518698dbaf331aa86431f85
ml7757 <matthew.w.laporte@gmail.com>
2016-03-05 06:34:56 +0100

    create methods for restaurants controller, populate seed file with test data

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,18 @@
 class RestaurantsController < ApplicationController
+
+  def index
+    restaurants = Restaurant.all
+    render json:{
+      restaurants: restaurants.as_json({include: :tags})
+    }
+  end
+
+  def show
+    restaurant = Restaurant.find(params[:id])
+    tags = restaurant.tags
+    render json:{
+      restaurant: restaurant,
+      tags: tags
+    }
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Tag.destroy_all
+Restaurant.destroy_all
+
+happy = Tag.create({option: "happy"})
+happy_meal = Tag.create({option: "Happy Meal"})
+burger = Tag.create({option: "burger"})
+
+mcdonalds = Restaurant.create({name: "McDonalds"})
+kfc = Restaurant.create({name: "KFC"})
+
+mcdonalds.tags << happy << happy_meal << burger
