0a1e3580919e2aa0960ded76e2fe4c9a4c02fa35
ml7757 <matthew.w.laporte@gmail.com>
2016-03-05 10:12:33 +0100

    update to cors file - errors pushing to heroku

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,4 +1,4 @@
-class TodoApi::Application
+class Follow::Application
   config.middleware.insert_before 0,  "Rack::Cors", debug: true, logger: (-> { Rails.logger }) do
     allow do
       origins 'localhost:3000', '127.0.0.1:3000',
